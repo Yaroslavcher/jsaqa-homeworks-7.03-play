d13c60e565d5c39593cbddbb74b8265893d1292b
unknown <yaroslavcher@gmail.com>
2023-01-24 23:08:23 +0300

    ready

--- tests/netology.test.js
+++ tests/netology.test.js
@@ -1,43 +1,38 @@
 const { test, expect } = require("@playwright/test");
-var validEmail = require("../user");
-var validPassword = require("../user");
+const user = require("../user");
 
-test("valid login", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("https://netology.ru/?modal=sign_in");
+  await page.getByPlaceholder("Email").fill(user.validEmail);
+});
 
-  // Input user email at Вход в личный кабинет
-
-  await page.getByPlaceholder("Email").fill(validEmail);
-
-  // Input user password at Вход в личный кабинет
-  await page.getByPlaceholder("Пароль").fill(validPassword);
-
-  // Click Войти
-  await page.getByTestId("login-submit-btn").click();
-
-  // Assert The profile page should opened
-  await expect(page).toHaveURL("https://netology.ru/profile");
-  await expect(
-    page.getByRole("heading", { name: "Мои курсы и профессии" })
-  ).toBeVisible();
-
+test.afterEach(async ({ page }) => {
   await page.close();
 });
 
-test("invalid login", async ({ page }) => {
-  await page.goto("https://netology.ru/?modal=sign_in");
+test.describe("Netology.ru login", () => {
+  test("valid login", async ({ page }) => {
+    // Input valid password at Вход в личный кабинет
+    await page.getByPlaceholder("Пароль").fill(user.validPassword);
 
-  // Input user email at Вход в личный кабинет
-  await page.getByPlaceholder("Email").fill(validEmail);
+    // Click Войти
+    await page.getByTestId("login-submit-btn").click();
 
-  // Input user password at Вход в личный кабинет
-  await page.getByPlaceholder("Пароль").fill("invalidP1!");
+    // Assert The profile page should opened
+    await expect(page).toHaveURL("https://netology.ru/profile");
+    await expect(
+      page.getByRole("heading", { name: "Мои курсы и профессии" })
+    ).toBeVisible();
+  });
 
-  // Click Войти
-  await page.getByTestId("login-submit-btn").click();
+  test("invalid login", async ({ page }) => {
+    // Input invalid password at Вход в личный кабинет
+    await page.getByPlaceholder("Пароль").fill(user.invalidPassword);
 
-  // Assert The profile page should not opened
-  await expect(page.getByTestId("login-error-hint")).toBeVisible();
+    // Click Войти
+    await page.getByTestId("login-submit-btn").click();
 
-  await page.close();
+    // Assert The profile page should not opened
+    await expect(page.getByTestId("login-error-hint")).toBeVisible();
+  });
 });
